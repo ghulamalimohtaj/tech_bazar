304b1a11ba8d27d29c9ab3ea07d6fc53ddb0dde7
ghulamalimohtaj <61561989+ghulamalimohtaj@users.noreply.github.com>
2021-04-14 06:49:57 +0430

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,7 @@
 <?php
-
+// connect to the database on clever-cloud
 $conn = new mysqli('bdfyhu9stn3ovnnljci1-mysql.services.clever-cloud.com','uueugxuxpjchtjyp','o1gpowZlsAoi50EGBT39','bdfyhu9stn3ovnnljci1');
 // $conn = new mysqli('localhost','root','','techbazar');
+// for persion inuts
 $conn->query("set names 'utf8'");
 ?>
