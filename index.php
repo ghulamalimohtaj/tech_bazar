0dc867f5f5cbd3d225f17c310bb96f966a7ee23a
ghulamalimohtaj <61561989+ghulamalimohtaj@users.noreply.github.com>
2021-04-17 04:04:14 +0430

    Update index.php
    enhance form validation

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 <title>TechBazar</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<!-- import external files -->
 <link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
 <link rel="stylesheet" href="layout/styles/w3.css">
 <link rel="stylesheet" href="layout/styles/font-awesome.min.css">
@@ -22,7 +21,7 @@
 <link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">-->
 </head>
 <body>
-    <!-- start content -->
+    
 <div class="wrapper row0" style="overflow: hidden;height: 60px">
   <div id="topbar" class="hoc clear"> 
     <div class="fl_left">
@@ -81,7 +80,7 @@
                           <input type="text" name="owner_lname" class="form-control"id="owner_lname" placeholder="Enter your lastname">
                         </div>
                         <div class="form-group">
-                          <label style="text-transform:lowercase;" for="">Phone(without +93 or 0)</label>
+                          <label for="">Phone(without +93 or 0)</label>
                           <input type="number" min="000000000" max="999999999" name="owner_phone" class="form-control" placeholder="Enter your phone number" required>
                         </div>
                         <div class="form-group">
@@ -102,7 +101,7 @@
                         <div class="form-group">
                           <label for="">Password</label>
                           <input type="password" name="owner_password" class="form-control" id="pass" placeholder="Enter your password" required>
-                          <p style="text-transform:lowercase;">password must be at least 7 characters long!</p>
+                          <p style="text-trnsform:lowercase;">The password must be at least 7 characters long!</p>
                         </div>
                         <div class="form-group">
                           <label for="">Confirm</label>
@@ -111,6 +110,7 @@
                         <div class="form-group">
                             <input type="submit" name="sned" value="Register" class="btn btn-primary" id="submit">
                         </div>
+                        <p onclick="validate()">fire</p>
                     </form>
                 </div>
         </div>
@@ -126,7 +126,6 @@
 </div>
     
      <?php
-    // show message after signing up to say if it was successful or not
         if(isset($_GET['reg'])){
             if($_GET['reg'] == 'ok'){
             echo ("<script>alert('successfull submission! you will be approved within 24 hours!');</script>");  
@@ -163,19 +162,16 @@
       
     <div class="row">
             <?php
-            // if the website is searched the query the searching word
             if(isset($_GET['q'])){
                 $q = $_GET['q'];
                 $getComputers = "SELECT * FROM computer WHERE model LIKE '%".$q."%' OR ram LIKE '%".$q."%' OR storage LIKE '%".$q."%' OR charge LIKE '%".$q."%' OR price LIKE '%".$q."%'";
               }else{
-                // defualt mode
             $getComputers = "SELECT * FROM computer ORDER BY add_date DESC LIMIT 6";
             }
             require 'connection.php';
             $exe = $conn->query($getComputers);
             if ($exe && $exe->num_rows > 0) {
                 while ($pc = $exe->fetch_assoc()) {
-                    // fetching all computers from the database
                         $shop_id = $pc['shop_id'];
                         $getMarket = "SELECT * FROM market WHERE mkt_id=".$pc['shop_id'];
                         $exeGetMember = $conn->query($getMarket);
@@ -197,7 +193,6 @@
 
                                     while($images = $sql->fetch_assoc()){
                                         if($i ==0){
-                                            // the fist computer picture will appear larger than others
                                             echo "<img src='market/pc/".$images['name']."' class='w3-full pc_images' onclick='showMe(this.src)'  data-toggle='modal' data-target='#show_img'>";
                                         }else{
                                             echo "<img src='market/pc/".$images['name']."' class='w3-third p-1 pc_images' onclick='showMe(this.src)'  data-toggle='modal' data-target='#show_img'>";
@@ -210,7 +205,6 @@
                                 </div>
                                 <br>
                             <div class="cursor-pointer" style="display:block;text-align:left">
-                                <!-- printing computers -->
                                 <h1 style="clear:both;text-transform:<?php if($pc['model'] == "hp") echo 'lowercase;';?>;"><?php echo($pc['model']); ?></h1>
                                 <p><?php echo("RAM: ".$pc['ram']); ?><br>
                                 <?php echo("Storage: ".$pc['storage']); ?><br>
@@ -247,7 +241,6 @@
 
 
 <?php
- // attach footer 
 require("footer.php");
 ?>
 <a id="backtotop" href="#top"><i class="fas fa-chevron-up"></i></a>
@@ -282,7 +275,6 @@ require("footer.php");
       xmlhttp.send();
     }
   }
-    // sign up form validation
   function validate(){
     var mkt_name = document.getElementById('mkt_name').value;
     var mkt_add = document.getElementById('mkt_add').value;
@@ -290,7 +282,7 @@ require("footer.php");
     var owner_lname = document.getElementById('owner_lname').value;
     var prof = document.getElementById('prof').value;
     var license = document.getElementById('license').value;
-    let myRegex = /[0-9!@#$%^&*+=/"'-_.>?{}<]/ig;
+    let myRegex = /[^A-z ]/ig;
     let addressRegex = /[!@#$%^&*+=/.>?{}<]/ig;
 
     var mkt_name_g = document.getElementById('mkt_name_g');
@@ -300,12 +292,12 @@ require("footer.php");
     
     var mkt_name_err = document.getElementById('mkt_name_err');
     var mkt_add_err = document.getElementById('mkt_add_err');
-    var woner_name_err = document.getElementById('owner_name_err');
+    var owner_name_err = document.getElementById('owner_name_err');
     var woner_lname_err = document.getElementById('owner_lname_err');
     //photos
     var license_err = document.getElementById('license_err');
     var pro_err = document.getElementById('pro_err');
-
+ 
     let mkt_name_result = mkt_name.match(myRegex);
     let mkt_add_result = mkt_add.match(addressRegex);
     let owner_name_result = owner_name.match(myRegex);
@@ -316,7 +308,7 @@ require("footer.php");
     var owner_name_flag = true;
     var owner_lname_flag = true;
 
-    if(mkt_name_result != null && (mkt_name.length<5 || mkt_name.length > 40)){
+    if(mkt_name_result != null || mkt_name.length<5 || mkt_name.length > 40){
       mkt_name_flag = false;
       mkt_name_err.innerHTML = "Invalid Name!";
       mkt_name_g.style.border = "1px solid red";
@@ -325,8 +317,8 @@ require("footer.php");
       mkt_name_err.innerHTML = "";
       mkt_name_g.style.border = "none";
     }
-
-    if(mkt_add_result != null){
+    
+    if(mkt_add_result != null || mkt_add_result.length<10 || mkt_add_result > 100){
       mkt_add_flag = false;
       mkt_add_err.innerHTML = "Invalid address!";
       mkt_add_g.style.border = "1px solid red";
@@ -336,19 +328,21 @@ require("footer.php");
       mkt_add_g.style.border = "none";
     }
     //Sayed Hagi mohammad kabir haidarabadi -->> the longest possible length for a name
-    if(owner_name_result != null && (owner_name.length < 3 || owner_name.length > 40)){
+    if(owner_name_result != null || owner_name.length < 3 || owner_name.length > 40){
+      
       owner_name_flag = false;
-      owner_name_err.innerHTML = "Invalid address!";
+      owner_name_err.innerHTML = "Invalid name!";
       owner_name_g.style.border = "1px solid red";
     }else{
+      
       owner_name_flag = true;
       owner_name_err.innerHTML = "";
       owner_name_g.style.border = "none";
     }
-
-    if(owner_lname_result != null && (owner_lname.length < 3 || owner_lname.length > 40)){
+ 
+    if(owner_lname_result != null || owner_lname.length < 3 || owner_lname.length > 40){
       owner_lname_flag = false;
-      owner_lname_err.innerHTML = "Invalid address!";
+      owner_lname_err.innerHTML = "Invalid last name!";
       owner_lname_g.style.border = "1px solid red";
     }else{
       owner_lname_flag = true;
@@ -360,7 +354,7 @@ require("footer.php");
   
         var slicePoint = prof.lastIndexOf('.')+1;
         var proExt = prof.slice(slicePoint,prof.length);
-        if(proExt == "jpg" || proExt == "png" || proExt == "jpeg" || proExt == "gif")  {
+        if(proExt == "jpg" || proExt == "png" || proExt == "JFIF" || proExt == "jpeg" || proExt == "gif")  {
           pro_img_flag = true;
           pro_err.innerHTML="";
           document.getElementById('prof').style.border = "none";
@@ -383,7 +377,7 @@ require("footer.php");
             document.getElementById('license').style.border = "1px solid red";
         }
     
-    // if everyting was valid then return true for submission otherwise false
+
     return mkt_name_flag && mkt_add_flag && owner_name_flag && owner_lname_flag && pro_img_flag && license_img_flag;
     
   }
