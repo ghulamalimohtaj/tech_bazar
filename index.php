7d1d2318e1b6fbf0439b9c11dc5e64eb29a37123
ghulamalimohtaj <61561989+ghulamalimohtaj@users.noreply.github.com>
2021-04-18 05:12:26 +0430

    Update index.php
    Add comments

--- index.php
+++ index.php
@@ -37,95 +37,88 @@
         <li><span id="fire-sign-in"><a href="login.php"  data-toggle="modal" data-target="#signup" >Sign up</a></span></li>
       </ul>
         
-        <div class="container">
-
+ <div class="container">
   <!-- The Modal -->
-   
 </div>
 
-        <div class="container mt-3">
-<style>
-.form-group{
-  padding:0px 10px 0px 10px;
-  border-radius:5px;
-}
-</style>
-  <div class="modal fade" id="signup">
-    <div class="modal-dialog">
-      <div class="modal-content" >
-      
-        
-        <!-- Modal body -->
-        <div class="modal-body" style="color:black">
-        <div >
-            <form method="post" action="signup.php" onsubmit="return validate()" enctype="multipart/form-data">
-                        <div class="form-group" id="mkt_name_g">
-                          <p id="mkt_name_err" class="error"></p>
-                          <label for="">Shop Name</label>
-                          <input type="text" name="mkt_name" class="form-control" id="mkt_name" placeholder="Enter your shop name" required>
-                        </div>
-                        <div class="form-group" id="mkt_add_g">
-                          <p id="mkt_add_err" class="error"></p>
-                          <label for="">Address</label>
-                          <input type="text" name="mkt_add" class="form-control" id="mkt_add" placeholder="Enter your addrss" required>
-                        </div>
-                        <div class="form-group" id="owner_name_g">
-                          <p id="owner_name_err" class="error"></p>
-                          <label for="">Name</label>
-                          <input type="text" name="owner_name" class="form-control" id="owner_name" placeholder="Enter your name" required>
-                        </div>
-                        <div class="form-group" id="owner_lname_g">
-                          <p id="owner_lname_err" class="error"></p>
-                          <label for="">Lastname</label>
-                          <input type="text" name="owner_lname" class="form-control"id="owner_lname" placeholder="Enter your lastname">
-                        </div>
-                        <div class="form-group">
-                          <label for="">Phone(without +93 or 0)</label>
-                          <input type="number" min="000000000" max="999999999" name="owner_phone" class="form-control" placeholder="Enter your phone number" required>
-                        </div>
-                        <div class="form-group">
-                          <label for="">email</label>
-                          <input type="email" name="owner_email" class="form-control" placeholder="Enter your email address" required>
-                        </div>
-                        
-                        <div class="form-group">
-                          <p id="pro_err" class="error"></p>
-                          <label for="">Photo</label>
-                          <input type="file" name="photo" id="prof" class="form-control">
-                        </div>
-                        <div class="form-group">
-                          <p id="license_err" class="error"></p>
-                          <label for="">License</label>
-                          <input type="file" name="license" id="license" class="form-control" required="">
-                        </div>
-                        <div class="form-group">
-                          <label for="">Password</label>
-                          <input type="password" name="owner_password" class="form-control" id="pass" placeholder="Enter your password" required>
-                          <p style="text-trnsform:lowercase;">The password must be at least 7 characters long!</p>
-                        </div>
-                        <div class="form-group">
-                          <label for="">Confirm</label>
-                          <input type="password" name="confirm" onkeyup="decide()" class="form-control" id="repass" placeholder="Retype your password" required>
-                        </div>
-                        <div class="form-group">
-                            <input type="submit" name="sned" value="Register" class="btn btn-primary" id="submit">
-                        </div>
-                        <p onclick="validate()">fire</p>
-                    </form>
-                </div>
+    <div class="container mt-3">
+    <style>
+    .form-group{
+      padding:0px 10px 0px 10px;
+      border-radius:5px;
+    }
+    </style>
+    <!-- Sign up form --> 
+      <div class="modal fade" id="signup">
+        <div class="modal-dialog">
+          <div class="modal-content" >
+            <!-- Modal body -->
+            <div class="modal-body" style="color:black">
+            <div > 
+                <form method="post" action="signup.php" onsubmit="return validate()" enctype="multipart/form-data">
+                    <div class="form-group" id="mkt_name_g">
+                      <p id="mkt_name_err" class="error"></p>
+                      <label for="">Shop Name</label>
+                      <input type="text" name="mkt_name" class="form-control" id="mkt_name" placeholder="Enter your shop name" required>
+                    </div>
+                    <div class="form-group" id="mkt_add_g">
+                      <p id="mkt_add_err" class="error"></p>
+                      <label for="">Address</label>
+                      <input type="text" name="mkt_add" class="form-control" id="mkt_add" placeholder="Enter your addrss" required>
+                    </div>
+                    <div class="form-group" id="owner_name_g">
+                      <p id="owner_name_err" class="error"></p>
+                      <label for="">Name</label>
+                      <input type="text" name="owner_name" class="form-control" id="owner_name" placeholder="Enter your name" required>
+                    </div>
+                    <div class="form-group" id="owner_lname_g">
+                      <p id="owner_lname_err" class="error"></p>
+                      <label for="">Lastname</label>
+                      <input type="text" name="owner_lname" class="form-control"id="owner_lname" placeholder="Enter your lastname">
+                    </div>
+                    <div class="form-group">
+                      <label for="">Phone(without +93 or 0)</label>
+                      <input type="number" min="000000000" max="999999999" name="owner_phone" class="form-control" placeholder="Enter your phone number" required>
+                    </div>
+                    <div class="form-group">
+                      <label for="">email</label>
+                      <input type="email" name="owner_email" class="form-control" placeholder="Enter your email address" required>
+                    </div>
+                    <div class="form-group">
+                      <p id="pro_err" class="error"></p>
+                      <label for="">Photo</label>
+                      <input type="file" name="photo" id="prof" class="form-control">
+                    </div>
+                    <div class="form-group">
+                      <p id="license_err" class="error"></p>
+                      <label for="">License</label>
+                      <input type="file" name="license" id="license" class="form-control" required="">
+                    </div>
+                    <div class="form-group">
+                      <label for="">Password</label>
+                      <input type="password" name="owner_password" class="form-control" id="pass" placeholder="Enter your password" required>
+                      <p style="text-trnsform:lowercase;">The password must be at least 7 characters long!</p>
+                    </div>
+                    <div class="form-group">
+                      <label for="">Confirm</label>
+                      <input type="password" name="confirm" onkeyup="decide()" class="form-control" id="repass" placeholder="Retype your password" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" name="sned" value="Register" class="btn btn-primary" id="submit">
+                    </div>
+                </form>
+            </div>
+           </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  
-</div>
-      
-    </div>
-
+   </div>
   </div>
 </div>
     
      <?php
+         // Show message wheather signing up was successful or not
         if(isset($_GET['reg'])){
             if($_GET['reg'] == 'ok'){
             echo ("<script>alert('successfull submission! you will be approved within 24 hours!');</script>");  
@@ -134,19 +127,24 @@
         }
         }
     ?>
+<!-- start header area -->
 <div class="">
   <header id="header" class="hoc col-12 row container p-4"> 
     <div class="w3-half">
       <h1 class="logoname container-fluid"><a href="index.html" id="title"><span>Tech</span>Bazar</a></h1>
     </div>
+    <!-- Search form -->
     <div class="w3-half ">
         <form method="get" action="index.php" class="search w3-half fr">
+           <!-- keep searched word after the page was refreshed by searching -->
           <input type="text" name="q" value="<?php if(isset($_GET['q']))echo($_GET['q']);?>" placeholder="search">
         </form>
     </div>
-        
   </header>
 </div>
+<!-- End header area -->
+
+<!-- Start background image area -->
 <div class="wrapper bgded overlay" style="<?php if(isset($_GET['q']))echo("display:none;");else{echo("display:block;");}?>background-image:url('images/demo/backgrounds/computer.jpg');">
   <div id="pageintro" class="hoc clear"> 
     <article>
@@ -154,24 +152,32 @@
     </article>
   </div>
 </div>
+<!-- End background image area -->
+
+<!-- Start computers section area -->
 <div class="wrapper row3">
   <main class="hoc container clear">
     <div class="sectiontitle">
-      <h3 class="" style="<?php if(isset($_GET['q']))echo("display:none;");else{echo("display:block;");}?>">Recently posted computers</h3>
+      <h3 class="" style="<?php if(isset($_GET['q']))echo("display:none;");else{echo("display:block;");} /* Hide background image while searching */?>">Recently posted computers</h3>
     </div>
-      
+    
     <div class="row">
             <?php
-            if(isset($_GET['q'])){
+            // if 
+            if(isset($_GET['q'])){// in searching mode
                 $q = $_GET['q'];
                 $getComputers = "SELECT * FROM computer WHERE model LIKE '%".$q."%' OR ram LIKE '%".$q."%' OR storage LIKE '%".$q."%' OR charge LIKE '%".$q."%' OR price LIKE '%".$q."%'";
               }else{
+            // Query all computers from the database
             $getComputers = "SELECT * FROM computer ORDER BY add_date DESC LIMIT 6";
             }
+            //connect to the database
             require 'connection.php';
             $exe = $conn->query($getComputers);
             if ($exe && $exe->num_rows > 0) {
                 while ($pc = $exe->fetch_assoc()) {
+                    
+                    // fetch all computer properties
                         $shop_id = $pc['shop_id'];
                         $getMarket = "SELECT * FROM market WHERE mkt_id=".$pc['shop_id'];
                         $exeGetMember = $conn->query($getMarket);
@@ -184,8 +190,8 @@
                         $market_address = $market['mkt_address'];
                     
                     ?>
-                            <div class="col-lg-4 col-md-4 col-sm-12 w3-card" id="pc" style="">
-                                      
+                           <!-- Each computer area which is repeated by the php while loop -->
+                            <div class="col-lg-4 col-md-4 col-sm-12 w3-card" id="pc" >         
                             <div style="width:100%;height:50%;">
                                 <?php
                                 $i =0;
@@ -204,6 +210,7 @@
                                 ?>
                                 </div>
                                 <br>
+                                <!-- show computer properties -->
                             <div class="cursor-pointer" style="display:block;text-align:left">
                                 <h1 style="clear:both;text-transform:<?php if($pc['model'] == "hp") echo 'lowercase;';?>;"><?php echo($pc['model']); ?></h1>
                                 <p><?php echo("RAM: ".$pc['ram']); ?><br>
@@ -224,38 +231,45 @@
 
     </div>
     <div class="modal fade" id="show_img">
-                        <div class="modal-dialog" style="width:80%">
-                            <span type="button" data-dismiss="modal" class="w3-button w3-black" style="cursor:pointer;position:absolute;top:0px">&cross;</span>
-                            <img src="" alt="" id="modal_img" style="width:100%">
-                        </div>
-                    </div>
+        <div class="modal-dialog" style="width:80%">
+            <span type="button" data-dismiss="modal" class="w3-button w3-black" style="cursor:pointer;position:absolute;top:0px">&cross;</span>
+            <img src="" alt="" id="modal_img" style="width:100%">
+        </div>
+    </div>
+      
+      <!-- Start About areat -->
     <div class="wrapper container about" id="about">
-  <div class="w3-center p-3">
-      <h1>About us</h1>
-      <p>TechBazar is an advertising platform for computer's sellers to upload thier compters' information in order to be available to customers</p>
-  </div>
-</div>
+      <div class="w3-center p-3">
+          <h1>About us</h1>
+          <p>TechBazar is an advertising platform for computer's sellers to upload thier compters' information in order to be available to customers</p>
+      </div>
+    </div>
+      <!-- End About area -->
+      
   </main>
 </div>
+<!-- End Main Section -->
 
 
 
 <?php
+// Attach Footer at the bottom of the the page 
 require("footer.php");
 ?>
+    
 <a id="backtotop" href="#top"><i class="fas fa-chevron-up"></i></a>
 <script type="text/javascript">
     var submit = document.getElementById('submit');
     submit.disabled=true;
+    // Enable submit button if password and conform password mached
     function decide(){
-
-    var pass = document.getElementById('pass');
-    var repass = document.getElementById('repass');
-   if(pass.value==repass.value && pass.value.length>6){
-     submit.disabled=false;
-   }else{
-     submit.disabled=true;
-   }
+        var pass = document.getElementById('pass');
+        var repass = document.getElementById('repass');
+       if(pass.value==repass.value && pass.value.length>6){
+         submit.disabled=false;
+       }else{
+         submit.disabled=true;
+       }
  }
 </script>
 <!-- JAVASCRIPTS -->
@@ -264,6 +278,7 @@ require("footer.php");
 <script src="layout/scripts/jquery.mobilemenu.js"></script>
 <script type="text/javascript">
  
+    // Form Validation 
   function validate(){
     var mkt_name = document.getElementById('mkt_name').value;
     var mkt_add = document.getElementById('mkt_add').value;
@@ -296,8 +311,6 @@ require("footer.php");
     var mkt_add_flag = true;
     var owner_name_flag = true;
     var owner_lname_flag = true;
-    alert(mkt_name_result);
-    alert(owner_name_result);
 
     if(mkt_name_result != null || mkt_name.length<5 || mkt_name.length > 40){
       mkt_name_flag = false;
@@ -368,7 +381,7 @@ require("footer.php");
             document.getElementById('license').style.border = "1px solid red";
         }
     
-
+    // Send form if everything was ok
     return mkt_name_flag && mkt_add_flag && owner_name_flag && owner_lname_flag && pro_img_flag && license_img_flag;
     
   }
